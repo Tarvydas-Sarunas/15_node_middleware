ca34f227b7c759f1ad71ebc47e80f960bb0a4c4f
SarunasT <sarunas.tarvydas@hotmail.com>
2023-12-26 20:35:18 +0100

    our middleware

--- src/server.js
+++ src/server.js
@@ -4,19 +4,47 @@ const morgan = require('morgan');
 const cors = require('cors');
 const mysql = require('mysql2/promise');
 const { dbConfig } = require('./config');
+const { dbQueryWithData } = require('./helper');
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+// my own middleware
+const logHello = (req, res, next) => {
+  console.log('--- Hi there -- Im -- middleware!!! ---');
+  // nextas leidzia kodui vykti toliau
+  next();
+};
+
+const reqTime = (req, res, next) => {
+  const now = new Date();
+  const time = now.toTimeString();
+  console.log('request:', time);
+  // nextas leidzia kodui vykti toliau
+  next();
+};
+
+// Middleware - app level
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(logHello);
+// app.use(reqTime);
 
-// ROUTES
+// routes
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+// MAIN ROUTES
+
+// get all posts   route level middleware
+app.get('/api/posts', reqTime, async (req, res) => {
+  const sql = 'SELECT * FROM posts';
+  const [rows, error] = await dbQueryWithData(sql, (argArr = []));
+  console.log('error ===', error);
+  res.json(rows);
 });
 
 // connect
@@ -33,7 +61,7 @@ async function testConnection() {
     if (conn) conn.end();
   }
 }
-testConnection();
+// testConnection();
 
 // app.listen(PORT);
 app.listen(PORT, () => {
